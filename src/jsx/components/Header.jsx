d0850731fda47d8f2fab1ef0d85d77a9288da1df
jhosuapp <jhosuapvll@gmail.com>
2022-12-09 16:39:05 -0500

    config libraries && header + dark mode

--- src/jsx/components/Header.jsx
+++ src/jsx/components/Header.jsx
@@ -1,8 +1,68 @@
+import '../../sass/layaout/_header.scss';
+import {useState} from 'react';
+//font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode, faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
 export const Header = ()=>{
-    return (
-      <header>
-          <p>hola</p>
-      </header>
-    )
+
+  const [isActive, setIsActive] = useState(false);
+  const getRootCtn = document.querySelector('body');
+  const getStateDarkMode = localStorage.getItem('dark-mode');
+
+  const toggleBtn = (e)=>{
+    setIsActive(current => !current);
+    localStorage.getItem('dark-mode') ? localStorage.removeItem('dark-mode') :  localStorage.setItem('dark-mode', true);
+  }
+  getStateDarkMode ? getRootCtn.classList.add('active') : getRootCtn.classList.remove('active');
+
+  return (
+    <header className='header'>
+      <div className="header-container">
+        <a href="#" className="header-logo">
+          <FontAwesomeIcon icon={ faCode }/>
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <a href="">Docs</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Sites</a>
+            </li>
+            <li>
+              <a href="">Sites</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header-block">
+          <div className="header-lng">
+            <div className='header-lng__icon'>
+              <FontAwesomeIcon icon={ faLanguage }/>
+            </div>
+            <div className="header-lng__select">
+              <p>Español</p>
+              <p>Ingles</p>
+            </div>
+          </div>
+          <div className={isActive ? 'active header-mode' : 'header-mode'} onClick={ toggleBtn }>
+            <span>
+              <FontAwesomeIcon icon={ faMoon }/>
+              <FontAwesomeIcon icon={ faSun }/>
+            </span>
+          </div>
+        </div>
+        <div className="header-hamburguer">
+
+        </div>
+      </div>
+    </header>
+  )
 }
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Header } from '../jsx/components/Header';
 
+//GLOBAL STYLES
+import '../sass/main.scss'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
